8226bcbcde183e39509a85e9ee690f16fc45ef4d
Hugo-Malafosse <101881646+Hugo-Malafosse@users.noreply.github.com>
2022-03-25 14:16:05 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -6,98 +6,11 @@ from typing import List
 
 import matplotlib.pyplot as plt
 import networkx as nx
+import numpy as np
 from mpl_toolkits.basemap import Basemap
 
 import prioqueue
 from Graphe import Graphe
-
-##########################################################################################
-
-def add_edge(adj: List[List[int]],
-             src: int, dest: int) -> None:
-    adj[src].append(dest)
-    adj[dest].append(src)
-
-
-# Function which finds all the paths
-# and stores it in paths array
-def find_paths(paths: List[List[int]], path: List[int],
-               parent: List[List[int]], n: int, u: int) -> None:
-    # Base Case
-    if (u == -1):
-        paths.append(path.copy())
-        return
-
-    # Loop for all the parents
-    # of the given vertex
-    for par in parent[u]:
-        # Insert the current
-        # vertex in path
-        path.append(u)
-
-        # Recursive call for its parent
-        find_paths(paths, path, parent, n, par)
-
-        # Remove the current vertex
-        path.pop()
-
-
-# Function which performs bfs
-# from the given source vertex
-def bfs(adj: List[List[int]],
-        parent: List[List[int]], n: int,
-        start: int) -> None:
-    # dist will contain shortest distance
-    # from start to every other vertex
-    dist = [maxsize for _ in range(n)]
-    q = deque()
-
-    # Insert source vertex in queue and make
-    # its parent -1 and distance 0
-    q.append(start)
-    parent[start] = [-1]
-    dist[start] = 0
-
-    # Until Queue is empty
-    while q:
-        u = q[0]
-        q.popleft()
-        for v in adj[u]:
-            if (dist[v] > dist[u] + 1):
-
-                # A shorter distance is found
-                # So erase all the previous parents
-                # and insert new parent u in parent[v]
-                dist[v] = dist[u] + 1
-                q.append(v)
-                parent[v].clear()
-                parent[v].append(u)
-
-            elif (dist[v] == dist[u] + 1):
-
-                # Another candidate parent for
-                # shortes path found
-                parent[v].append(u)
-
-
-# Function which prints all the paths
-# from start to end
-def print_paths(adj: List[List[int]], n: int,
-                start: int, end: int) -> None:
-    paths = []
-    path = []
-    parent = [[] for _ in range(n)]
-
-    # Function call to bfs
-    bfs(adj, parent, n, start)
-
-    # Function call to find_paths
-    find_paths(paths, path, parent, n, end)
-    paths_unique = []
-    for v in paths:
-        if v not in paths_unique:
-            paths_unique.append(v)
-    return paths_unique
 
 ######################################################################################################
 # Driver Code
@@ -106,10 +19,6 @@ file = open("edge.csv")
 csvreader = csv.reader(file)
 
 header = next(csvreader)
-
-
-
-
 
 rows = []
 for row in csvreader:
@@ -148,7 +57,6 @@ for row in rows2:
 
     row_norm2.append(rowB)
 
-
 file1 = open("noeux.csv")
 csvreader1 = csv.reader(file1)
 
@@ -177,7 +85,105 @@ nombre_demande = len(row_norm2)
 
 weights = [row_norm[i][3] for i in range(nombre_edge)]
 
+
 ###########################################################################################################
+
+##########################################################################################
+def findindexedge(u, v):
+    for rowa in row_norm:
+        if ((u == rowa[1] and v == rowa[2])
+                or (u == rowa[2] and v == rowa[1])):
+            return rowa[0] - 1
+
+
+def add_edge(adj: List[List[int]],
+             src: int, dest: int) -> None:
+    adj[src].append(dest)
+    adj[dest].append(src)
+
+
+# Function which finds all the paths
+# and stores it in paths array
+def find_paths(paths: List[List[int]], path: List[int],
+               parent: List[List[int]], n: int, u: int) -> None:
+    # Base Case
+    if (u == -1):
+        paths.append(path.copy())
+        return
+
+    # Loop for all the parents
+    # of the given vertex
+    for par in parent[u]:
+        # Insert the current
+        # vertex in path
+        path.append(u)
+
+        # Recursive call for its parent
+        find_paths(paths, path, parent, n, par)
+
+        # Remove the current vertex
+        path.pop()
+
+
+# Function which performs bfs
+# from the given source vertex
+def bfs(adj: List[List[int]],
+        parent: List[List[int]], n: int,
+        start: int, weight) -> None:
+    # dist will contain shortest distance
+    # from start to every other vertex
+    dist = [maxsize for _ in range(n)]
+    q = deque()
+
+    # Insert source vertex in queue and make
+    # its parent -1 and distance 0
+    q.append(start)
+    parent[start] = [-1]
+    dist[start] = 0
+
+    # Until Queue is empty
+    while q:
+        u = q[0]
+        q.popleft()
+        for v in adj[u]:
+            if (dist[v] > dist[u] + 1):
+
+                # A shorter distance is found
+                # So erase all the previous parents
+                # and insert new parent u in parent[v]
+                distance1 = weight[findindexedge(v, u)]
+                dist[v] = dist[u] + distance1
+                q.append(v)
+                parent[v].clear()
+                parent[v].append(u)
+
+            elif (dist[v] == dist[u] + 1):
+
+                # Another candidate parent for
+                # shortes path found
+                parent[v].append(u)
+
+
+# Function which prints all the paths
+# from start to end
+def print_paths(adj: List[List[int]], n: int,
+                start: int, end: int, weight) -> None:
+    paths = []
+    path = []
+    parent = [[] for _ in range(n)]
+
+    # Function call to bfs
+    bfs(adj, parent, n, start, weight)
+
+    # Function call to find_paths
+    find_paths(paths, path, parent, n, end)
+    paths_unique = []
+    for v in paths:
+        if v not in paths_unique:
+            paths_unique.append(v)
+    return paths_unique
+
+
 #################################################################################################################
 
 
@@ -191,7 +197,6 @@ def constru_graphe():
         graphe[row[2]].append((row[1], row[3]))
 
     return graphe
-
 
 
 Blanc = 0
@@ -271,6 +276,7 @@ def dijkstra(graph, sourceNode):
     # We return the distances
     return distances
 
+
 graphe_arrete = constru_graphe()
 
 gex = Graphe(graphe_arrete)
@@ -321,6 +327,7 @@ def tous_chemins(peres, r):
         if peres[i] != -1:
             cs.append(chemin(peres, i, r))
     return cs
+
 
 #################################################################################################################
 #################################################################################################################
@@ -475,7 +482,7 @@ def Loss_weight(weight):
 
         demande = row[2]
 
-        paths_unique = print_paths(adj, n, nodeA, nodeB)
+        paths_unique = print_paths(adj, n, nodeA, nodeB, weight)
 
         for path in paths_unique:
 
@@ -492,72 +499,125 @@ def Loss_weight(weight):
     for charge in CHARGES_weight:
         charges_tot_weight.append(int(10 * sum(charge)) / 10)
 
-
-    print(charges_tot_weight)
-
     charge_capacite = []
     for i in range(len(charges_tot_weight)):
-        charge_capacite.append(int(10*charges_tot_weight[i]/row_norm[i][4])/10)
-
-
-    print(charge_capacite)
-
+        charge_capacite.append(int(10 * charges_tot_weight[i] / row_norm[i][4]) / 10)
 
     MAX = max(charge_capacite)
     iMAX = charge_capacite.index(MAX) + 1
-    print(MAX, iMAX)
+    #print(MAX)
+    #print(iMAX)
+    #print(charge_capacite)
+
     return iMAX, MAX, charge_capacite
 
 
-indice, Loss, charges_tot_weight = Loss_weight(weights)
+
+##############################################################################################
+
+def afficher_graphe(weight):
+    indice, Loss, charges_tot_weight = Loss_weight(weight)
+    G1 = nx.Graph()
+    for i in range(nombre_noeuds):
+        G1.add_node(i, label=row_norm1[i][2], col='blue')
+
+    for i in range(nombre_edge):
+        G1.add_edge(row_norm[i][1], row_norm[i][2], weight=charges_tot_weight[row_norm[i][0] - 1]
+                    , styl='solid')
+
+    pos = {i: (float(row_norm1[i][5]), float(row_norm1[i][4]))
+           for i in range(nombre_noeuds)}
+
+    liste = list(G1.nodes(data='col'))
+    colorNodes = {}
+    for noeud in liste:
+        colorNodes[noeud[0]] = noeud[1]
+
+    colorList = [colorNodes[node] for node in colorNodes]
+
+    liste = list(G1.nodes(data='label'))
+    labels_nodes = {}
+    for noeud in liste:
+        labels_nodes[noeud[0]] = noeud[1]
+
+    labels_edges = {}
+    labels_edges = {edge: G1.edges[edge]['weight'] for edge in G1.edges}
+
+    # nodes
+    nx.draw_networkx_nodes(G1, pos, node_size=100, node_color=colorList, alpha=0.9)
+
+    # labels
+    nx.draw_networkx_labels(G1, pos, labels=labels_nodes,
+                            font_size=15,
+                            font_color='black',
+                            font_family='sans-serif')
+
+    # edges
+    nx.draw_networkx_edges(G1, pos, width=3)
+    nx.draw_networkx_edge_labels(G1, pos, edge_labels=labels_edges, font_color='red')
+    plt.axis('off')
+    plt.savefig('fig1.png')
+
+    plt.show()
+
 ###############################################################################################
+
+def df(i, weight):
+    dweight = weight.copy()
+    dweight[i] += 10
+    indicedf, Lossdf, charges_tot_weightdf = Loss_weight(dweight)
+    indice1, Loss1, charges_tot_weight1 = Loss_weight(weight)
+
+    return (Lossdf - Loss1)
+
+
+def grad(weight):
+    gradient = []
+    for i in range(nombre_edge):
+        gradient.append(df(i, weight))
+    return gradient
+
+
+def un_pas(pas, weight):
+    weightnouv = np.array(weight) - np.array(pas * grad(weight))
+    weightint = []
+
+    for i in range(len(weightnouv)) :
+        weightint.append(int(weightnouv[i]) +2)
+
+
+
+    return weightint
+
+
+indice1, Loss, charges_tot_weight1 = Loss_weight(weights)
+print(weights)
+print(indice1, Loss, charges_tot_weight1)
+afficher_graphe(weights)
+
+
+
+
+while Loss>2:
+    weights = un_pas(1, weights)
+    print(weights)
+    indice2, Loss, charges_tot_weight2 = Loss_weight(weights)
+    print(Loss)
+
+print(weights)
+print(indice2, Loss, charges_tot_weight2)
+
+afficher_graphe(weights)
+
 #################################################################################################################
+
+
+
 #################################################################################################################
 
-G1 = nx.Graph()
-for i in range(nombre_noeuds):
-    G1.add_node(i, label=row_norm1[i][2], col='blue')
 
-for i in range(nombre_edge):
-    G1.add_edge(row_norm[i][1], row_norm[i][2], weight=charges_tot_weight[row_norm[i][0] - 1]
-                , styl='solid')
 
-pos = {i: (float(row_norm1[i][5]), float(row_norm1[i][4]))
-       for i in range(nombre_noeuds)}
-
-liste = list(G1.nodes(data='col'))
-colorNodes = {}
-for noeud in liste:
-    colorNodes[noeud[0]] = noeud[1]
-
-colorList = [colorNodes[node] for node in colorNodes]
-
-liste = list(G1.nodes(data='label'))
-labels_nodes = {}
-for noeud in liste:
-    labels_nodes[noeud[0]] = noeud[1]
-
-labels_edges = {}
-labels_edges = {edge: G1.edges[edge]['weight'] for edge in G1.edges}
-
-# nodes
-nx.draw_networkx_nodes(G1, pos, node_size=100, node_color=colorList, alpha=0.9)
-
-# labels
-nx.draw_networkx_labels(G1, pos, labels=labels_nodes,
-                        font_size=15,
-                        font_color='black',
-                        font_family='sans-serif')
-
-# edges
-nx.draw_networkx_edges(G1, pos, width=3)
-nx.draw_networkx_edge_labels(G1, pos, edge_labels=labels_edges, font_color='red')
-plt.axis('off')
-plt.savefig('fig1.png')
-
-plt.show()
-
-m = Basemap(width=12000000, height=9000000, projection='lcc',
+'''m = Basemap(width=12000000, height=9000000, projection='lcc',
             resolution='c', lat_1=45., lat_2=55, lat_0=50, lon_0=-107.)
 # draw coastlines.
 m.drawcoastlines()
@@ -567,4 +627,4 @@ m.drawcoastlines()
 m.drawmapboundary(fill_color='aqua')
 # fill continents, set lake color same as ocean color.
 m.fillcontinents(color='coral', lake_color='aqua')
-plt.show()
+plt.show()'''
